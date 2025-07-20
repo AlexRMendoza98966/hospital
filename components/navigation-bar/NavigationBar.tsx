ab330b35ecda81cd5802b02f7634e8fdc6b36e01
AlexRMendoza98966 <lusifago98@gmail.com>
2025-07-19 20:49:20 -0400

    arreglo css

---
+++ components/navigation-bar/NavigationBar.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import styles from './navigation.module.css';
+
+const NavigationBar: React.FC = () => {
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setSticky(window.scrollY > 0);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <nav className={`${styles.navbarMain} ${sticky ? styles.navbarSticky : ''}`}>
+      {/* ...existing navigation content... */}
+    </nav>
+  );
+};
+
+export default NavigationBar;
