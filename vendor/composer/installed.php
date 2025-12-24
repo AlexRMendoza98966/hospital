8383e39dcde1e397790e422823e4f6edfbf21c6b
AlexRMendoza98966 <lusifago98@gmail.com>
2025-12-24 18:15:40 -0400

    actualizando version de node y archivos new

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mi/hospital-home',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '365cede36b378dfbcf3feb30241a00d1529d219a',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mi/hospital-home' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '365cede36b378dfbcf3feb30241a00d1529d219a',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
