f40ac7ad917f449497159e9322e3abf4a8f353f0
AlexRMendoza98966 <lusifago98@gmail.com>
2026-06-19 17:03:23 -0400

    Fix Vercel Next.js deployment config

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hospitaldelnino.com.bo',
+      },
+    ],
   },
-  output: 'export',
-  trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/hospital' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/hospital/' : '',
 }
 
 export default nextConfig
